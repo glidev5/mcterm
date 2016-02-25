660020ba1f604d07f2798444328d2b2efc825803
Your Name <you@example.com>
2016-02-25 03:28:54 +0000

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+<snippet>
+  <content><![CDATA[
+# MCTERM
+Using computer craft mod is sweet as you can have computers in minecraft. But don't you want to have a ssh connection between computers inside minecraft and real world computers? Wait no more, mcterm is such a server that allows you to communicate between minecraft computer and real world computer through ssh connection.
+
+## Installation
+1. minecraft server:
+* install computer craft in both server and client
+* ensure http is enabled for server
+
+2. node server:
+git clone
+npm install
+npm run
+
+3. computer inside minecraft:
+* get market:
+pastebin get w1RtfqFd market
+market link available later
+
+## Usage
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+## History
+0.0.1  git init
+## Credits
+Thanks to Node.js for awesome modules.
+
+## License
+Apache License
+http://www.apache.org/licenses/LICENSE-2.0
+
+]]></content>
+  <tabTrigger>readme</tabTrigger>
+</snippet>
---
+++ web.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var SSH = require('simple-ssh');
+var app = express();
+var fs=require('fs');
+var needle=require('needle');
+var htmlToText = require('html-to-text');
+var Tail = require('tail').Tail;
+var stripAnsi = require('strip-ansi');
+
+var tailed="";
+var tail = new Tail("/root/screenlog.0");
+tail.on("line", function(data) {
+  tailed+=data;
+});
+
+tail.on("error", function(error) {
+  console.log('ERROR: ', error);
+});
+
+var html_option={
+  compressed         : true,
+  follow_max         : 5,
+  rejectUnauthorized : false
+};
+
+var text;
+
+app.get('/www',function(req,res){
+ needle.get(req.param("url"),html_option, function(error, response) {
+  if (!error){
+    text = htmlToText.fromString(response.body, {
+        wordwrap: 80
+    });
+    res.send(text);
+    console.log()
+  }
+ });
+});
+
+app.get('/json',function(req,res){
+ needle.get(req.param("url"),html_option, function(error, response) {
+  if (!error){
+    text = JSON.stringify(JSON.parse(response.body),null,2);
+    res.send(text);
+    console.log()
+  }
+ });
+});
+
+app.get('/wiki',function(req,res){
+ needle.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+req.param("query"),html_option, function(error, response) {
+  if (!error){
+    var text = JSON.stringify(response.body,null,2);
+    res.send(text);
+  }
+  else {
+    console.log(error);
+  }
+ });
+});
+
+var key=fs.readFileSync('/root/.ssh/id_rsa').toString();
+var ssh_option={
+    host: 'localhost',
+    user: 'root',
+    pass: '',
+    key: key,
+    agent: process.env.SSH_AUTH_SOCK,
+    agentForward: true
+  };
+
+app.get('/cmd', function(req,res){
+  var cmd=req.param('cmd');  
+
+  function out(stdout) {
+    console.log("cmd: "+cmd);
+    console.log(stdout);
+    res.send(stdout);
+  }
+
+  (new SSH(ssh_option)).exec(cmd, {out: out}).start();
+});
+
+app.get('/screen', function(req,res){
+  var cmd="screen -S "+req.param("screen")+" -X stuff '"+req.param('cmd')+"'`echo -ne '\015'`";
+
+  function out(stdout) {
+    console.log("screen: "+cmd);
+    console.log(stdout);
+    res.send("200 OK");
+  }
+
+  (new SSH(ssh_option)).exec(cmd, {out: out}).start();
+});
+
+app.get('/tail', function(req,res){
+  tailed=stripAnsi(tailed);
+  res.send(tailed);
+  if(tailed.trim()){console.log(tailed);}
+  tailed="";
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('mcterm server listening on port 3000!');
+});
+
+
+var ssh = new SSH({
+    host: 'localhost',
+    user: 'username',
+    pass: 'password'
+});
+ 
+ssh.exec('echo $PATH', {
+    out: function(stdout) {
+        console.log(stdout);
+    }
+}).start();
